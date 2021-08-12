cba357a0925f7fdc68d5788dbd553ecdc908b909
Jolusegun <Joyce.o.olusegun@gmail.com>
2021-08-11 21:08:00 -0500

    Showing recent temperature and attributes
    Showing wind, humidity, and temperature

---
+++ src/app.js
@@ -0,0 +1,149 @@
+function currentDate(datetime) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let hours = [
+    "00",
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+  ];
+
+  let minutes = [
+    "00",
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+    "29",
+    "30",
+    "31",
+    "32",
+    "33",
+    "34",
+    "35",
+    "36",
+    "37",
+    "38",
+    "39",
+    "40",
+    "41",
+    "42",
+    "43",
+    "44",
+    "45",
+    "46",
+    "47",
+    "48",
+    "49",
+    "50",
+    "51",
+    "52",
+    "53",
+    "54",
+    "55",
+    "56",
+    "57",
+    "58",
+    "59",
+  ];
+  let currentYear = datetime.getFullYear();
+  let currentDay = days[datetime.getDay()];
+  let currentMonth = months[datetime.getMonth()];
+  let currentDate = datetime.getDate();
+  let currentHour = hours[datetime.getHours()];
+  let currentMinute = minutes[datetime.getMinutes()];
+  let currentSecond = datetime.getSeconds();
+
+  return `<strong>${currentDay}</strong>, ${currentMonth} ${currentDate},${currentYear} ${currentHour}:${currentMinute}:${currentSecond}`;
+}
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "13180cac35345d5f25e6b2fd362d032f";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
+
+let dateElement = document.querySelector(".datetime");
+let currentTime = new Date();
+dateElement.innerHTML = currentDate(currentTime);
