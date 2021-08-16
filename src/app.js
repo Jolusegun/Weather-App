e1cdfec5e8290800a790d2f4d33c75ea3ddc4336
Jolusegun <Joyce.o.olusegun@gmail.com>
2021-08-15 21:21:41 -0500

    Got Forecast coding for js

--- src/app.js
+++ src/app.js
@@ -128,7 +128,7 @@ let dateElement = document.querySelector(".datetime");
 let currentTime = new Date();
 dateElement.innerHTML = currentDate(currentTime);
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thrus", "Fri", "Sat", "Sun", "Mon", "Tues"];
@@ -149,6 +149,12 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "13180cac35345d5f25e6b2fd362d032f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -174,6 +180,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -217,4 +225,3 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 search("Nashville");
-displayForecast();
