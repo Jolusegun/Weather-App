caf988523a339d14c6a9e5c57d32741c5222583b
Jolusegun <Joyce.o.olusegun@gmail.com>
2021-08-15 23:29:42 -0500

    Working link on cities

--- src/app.js
+++ src/app.js
@@ -113,15 +113,79 @@ function currentDate(datetime) {
     "58",
     "59",
   ];
+
+  let seconds = [
+    "00",
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+    "29",
+    "30",
+    "31",
+    "32",
+    "33",
+    "34",
+    "35",
+    "36",
+    "37",
+    "38",
+    "39",
+    "40",
+    "41",
+    "42",
+    "43",
+    "44",
+    "45",
+    "46",
+    "47",
+    "48",
+    "49",
+    "50",
+    "51",
+    "52",
+    "53",
+    "54",
+    "55",
+    "56",
+    "57",
+    "58",
+    "59",
+  ];
+
   let currentYear = datetime.getFullYear();
   let currentDay = days[datetime.getDay()];
   let currentMonth = months[datetime.getMonth()];
   let currentDate = datetime.getDate();
   let currentHour = hours[datetime.getHours()];
   let currentMinute = minutes[datetime.getMinutes()];
-  let currentSecond = datetime.getSeconds();
+  let currentSecond = seconds[datetime.getSeconds()];
 
-  return `<strong>${currentDay}</strong>,${currentMonth} ${currentDate},${currentYear} ${currentHour}:${currentMinute}:${currentSecond}`;
+  return `<strong>${currentDay}</strong>,${currentMonth} ${currentDate}, ${currentYear} ${currentHour}:${currentMinute}:${currentSecond}`;
 }
 
 let dateElement = document.querySelector(".datetime");
@@ -175,7 +239,6 @@ function getForecast(coordinates) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
