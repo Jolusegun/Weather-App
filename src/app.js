0430d53792092936fedd331702a8c81e498bbdb8
Jolusegun <Joyce.o.olusegun@gmail.com>
2021-08-15 22:16:17 -0500

    Stored API with Variable
    Added code in JS for accurate date and weather. Implemented API, added max index, updated dummy data

--- src/app.js
+++ src/app.js
@@ -128,25 +128,42 @@ let dateElement = document.querySelector(".datetime");
 let currentTime = new Date();
 dateElement.innerHTML = currentDate(currentTime);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thrus", "Fri", "Sat", "Sun", "Mon", "Tues"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
             <div class="weather-forecast-date">
-            ${day}</div>
-            <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="partly cloudly" width="42"/>
+            ${formatDay(forecastDay.dt)}</div>
+            <img src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png" alt="" width="42"/>
             <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max">18°</span> <span class="weather-forecast-temperature-min">12°</span>
+            <span class="weather-forecast-temperature-max">${Math.round(
+              forecastDay.temp.max
+            )}°</span> <span class="weather-forecast-temperature-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
             </div>
           </div>
         `;
+    }
   });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
